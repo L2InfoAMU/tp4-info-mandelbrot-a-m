04904ff8ec056d50ed8588f9ec10314db4c32998
Yash <yashthevalil@gmail.com>
2018-11-04 00:44:18 +0100

    divide OK

--- src/main/java/mandelbrot/Complex.java
+++ src/main/java/mandelbrot/Complex.java
@@ -165,13 +165,13 @@ public class Complex {
      * @return the complex number <code>this / divisor</code>
      */
     Complex divide(Complex divisor) {
-        if (divisor.equals(I)){
-            throw new ArithmeticException("divide by zero");
+        if (divisor.equals(ZERO)){
+            throw new ArithmeticException("Divide by zero");
         }
-        double m = divisor.squaredModulus();
+        double factor = 1 / (Math.pow(divisor.getReal(),2) + Math.pow(divisor.getImaginary(),2));
         return new Complex(
-                (this.real + divisor.real + this.imaginary + divisor.imaginary) / m,
-                (this.imaginary * divisor.real - this.real * divisor.imaginary) / m
+                factor * (real * divisor.getReal() + imaginary * divisor.getImaginary()),
+                factor * (imaginary * divisor.getReal() - real * divisor.getImaginary())
         );
     }
 
